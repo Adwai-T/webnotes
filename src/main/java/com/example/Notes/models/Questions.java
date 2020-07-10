5d9c76357e59544455e20e8dfb33671ed61572dc
Adwai-T <waitforadi@gmail.com>
2020-07-10 11:24:25 +0530

    fixed question not retrive bug

--- src/main/java/com/example/Notes/models/Questions.java
+++ src/main/java/com/example/Notes/models/Questions.java
@@ -5,9 +5,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collation = "quizs")
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Getter @Setter
 @NoArgsConstructor
